cfc6a96e6b767df0a24256347431ebe204e5bb32
West Lane <west@paperequator.com>
2018-04-18 12:58:26 -0400

    Device Config File
    - store config.yml for recurring use
    - improve support for app / db server restarts
    - split out wlan0 / wifi setup into stand-alone script
    - use encrypted wifi passwords when storing on disk
    - minor updates to file permissions

--- container/app/server.js
+++ container/app/server.js
@@ -1,18 +1,21 @@
+var fs = require("fs");
 var express = require("express");
 var path = require("path");
 var execSync = require("child_process").execSync;
+var yaml = require('js-yaml');
+
 var cors = require("./middleware/cors");
 var rewrite = require("./middleware/rewrite");
 var captive = require("./middleware/captive");
 var utils = require("./lib/utils");
 var Stor = require("./lib/stor");
-var db_config = require("./config.json");
 
 //----------------------------------------------------------------- App Server
 var app = express();
 var port = (process.env.TERM_PROGRAM ? 8000 : 80);
 app.disable("x-powered-by");
 var static_path = path.resolve(__dirname + "/public/");
+var config_file = __dirname + "/config.yml";
 
 
 //--------------------------------------------------------------------- Routes
@@ -24,6 +27,8 @@ app.use("/", express.static(static_path));
 console.log("[server] starting server...");
 
 utils.checkInternet(function(is_connected) {
+
+    // first, try to load latest web app...
     if (is_connected) {
         console.log("[server] internet access: active");
         console.log("[server] checking for updated web platform");
@@ -33,10 +38,21 @@ utils.checkInternet(function(is_connected) {
     else {
         console.log("[server] internet access: unavailable");
     }
-});
 
-app.listen(port, function() {
-    console.log("[server] ready on port %s ...", port);
-    var db_uri = "http://admin:"+db_config.admins.admin+"@localhost:" + port;
-    var db = new Stor(db_uri + "/db/lantern");
+    // connect to and setup database...
+    try {
+        var config = yaml.safeLoad(fs.readFileSync(config_file, 'utf8'));
+        var db_uri = "http://admin:"+config.DB_PASS+"@localhost:" + port;
+        var db = new Stor(db_uri + "/db/lantern");
+
+        // finally, start up server
+        app.listen(port, function() {
+            console.log("[server] ready on port %s ...", port);
+        });
+
+    } catch (e) {
+        console.log(e);
+    }
+
+
 });
