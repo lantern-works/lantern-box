bd6d3a78a3db581472967298da72d4697a4a625e
West Lane <west@paperequator.com>
2018-04-17 20:41:28 -0400

    More CORS
    - always accept localhost:3000

--- container/app/middleware/cors.js
+++ container/app/middleware/cors.js
@@ -1,11 +1,11 @@
 module.exports = function Cors(req, res, next) {
-    if (!process.env.ORIGINS) {
-        return next();
-    }
-    var allowedOrigins = process.env.ORIGINS.split(",");
+  
+    process.env.ORIGINS = process.env.ORIGINS +",http://localhost:3000";
+    var allowed_origins = process.env.ORIGINS.split(",");
     var origin = req.headers.origin;
-    if(allowedOrigins.indexOf(origin) > -1){
+    if(allowed_origins.indexOf(origin) > -1){
          res.setHeader('Access-Control-Allow-Origin', origin);
+         console.log(origin);
     }
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'accept, authorization, content-type, origin, referer, x-csrf-token');
