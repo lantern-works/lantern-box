e726aa1d9ca98b28d97361a508f6be2ceebdab68
West Lane <west@paperequator.com>
2018-04-24 16:56:13 -0400

    Expose _uuids route at root of server
    - fixes issue where documents could not be created inside _utils admin web app

--- container/app/middleware/rewrite.js
+++ container/app/middleware/rewrite.js
@@ -1,6 +1,6 @@
 // https://github.com/pouchdb/express-pouchdb/issues/116
 module.exports = function(req,res,next) {
-    var paths = ['/_session', '/_all_dbs', '/_replicator', 
+    var paths = ['/_session', '/_all_dbs', '/_replicator', "/_uuids", 
         '/_users', '/_utils', "/lantern"];
     for (var i=0; i<paths.length; i++) {
         if (req.url.indexOf(paths[i]) ===0) {
@@ -9,4 +9,4 @@ module.exports = function(req,res,next) {
         }
     }
     next();
-}
+};
