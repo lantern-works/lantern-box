2950ea1c79d7ed5b8f85a88bc3570b61d27c691a
West Lane <west@paperequator.com>
2018-04-29 19:36:44 -0400

    URL rewrite to ensure trailing slash
    - just /_utils/ for the time being

--- container/app/middleware/rewrite.js
+++ container/app/middleware/rewrite.js
@@ -1,11 +1,22 @@
 // https://github.com/pouchdb/express-pouchdb/issues/116
 module.exports = function(req,res,next) {
+
+    // PouchDB web admin may need access to some or all of these at the root
     var paths = ['/_session', '/_all_dbs', '/_replicator', "/_uuids", 
         '/_users', '/_utils', "/lantern"];
+
+    // if we're requesting one of these, make sure to prefix with "/db"
     for (var i=0; i<paths.length; i++) {
-        if (req.url.indexOf(paths[i]) ===0) {
-            req.url = req.originalUrl = "/db" + req.url;
-            return next();
+        if (req.url.indexOf(paths[i]) === 0) {
+            
+            // make sure we have trailing slash for certain paths
+            if (req.url == "/_utils") {
+                res.redirect(301, req.url+"/");
+            }
+            else {
+                req.url = req.originalUrl = "/db" + req.url;
+                return next();
+            }
         }
     }
     next();
