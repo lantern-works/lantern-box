973e405bd386050efad4e68d8b91835743721972
West Lane <west@paperequator.com>
2019-01-14 18:39:38 -0500

    Outgoing messages handled within server
    - uses new "hooks" available from lantern-serve to go directly to LoRa
    - no need to run independent change watcher service (saves on memory and simplifies processes)
    - renamed a few binaries for simplicity
    - use "sync" to refer to the way we leverage WiFi to connect across devices

--- init/network.sh
+++ init/network.sh
@@ -10,7 +10,7 @@ if [[ -f /boot/config.txt ]]; then
     sed -i '/^hosts: /s/files dns/files mdns dns/' /etc/nsswitch.conf
     ln -sf /usr/lib/systemd/system/avahi-daemon.service /etc/systemd/system/multi-user.target.wants/avahi-daemon.service
 
-    # enable wireless, actual connection details will be configured by the user, likely over usb-serial.
+    # enable wireless, actual connection details will be configured by the user
     ln -sf /usr/lib/systemd/system/netctl-auto@.service /etc/systemd/system/multi-user.target.wants/netctl-auto@wlan0.service
 else
     echo "skipping network setup within docker container..."    
